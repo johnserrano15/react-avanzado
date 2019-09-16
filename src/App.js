090a00357ddb7dc168b1f8a4123e5ca271ff7bda
John Serrano <andrey_201015@hotmail.com>
2019-09-16 09:58:15 -0500

    Add animations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 
 export const App = () => (
   <>
