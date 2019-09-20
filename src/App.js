20ecd968d59369f262dbd1e96a249bc4c6eaea10
John Serrano <andrey_201015@hotmail.com>
2019-09-20 15:27:37 -0500

    Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { Navbar } from './components/Navbar'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -16,6 +23,25 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:id' />
       </Router>
+
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? (
+                <Router>
+                  <Favs path='/favs' />
+                  <User path='/user' />
+                </Router>
+              )
+              : (
+                <Router>
+                  <NotRegisteredUser path='/favs' />
+                  <NotRegisteredUser path='/user' />
+                </Router>
+              )
+        }
+      </UserLogged>
       <Navbar />
     </>
   )
